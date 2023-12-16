fdfca0e6311499348b1d038073c4de9555549e37
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2023-12-16 21:04:49 +0530

    tye128

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -55,30 +55,80 @@
 
 /* eslint-disable no-undef */
 
+// const todoList = () => {
+//   const all = []
+
+//   const add = (todoItem) => {
+//     all.push(todoItem)
+//   }
+
+//   const markAsComplete = (index) => {
+//     all[index].completed = true
+//   }
+
+//   const overdue = () => {
+//     const today = new Date().toISOString().split('T')[0]
+//     return all.filter((item) => !item.completed && item.dueDate < today)
+//   }
+
+ 
+//   const dueToday = () => {
+//     const today = new Date().toISOString().split('T')[0];
+//     return all.filter(
+//       (item) =>
+//         item.dueDate === today &&
+//         (item.title === 'Pay rent' ? item.completed : true)
+//     );
+//   };
+
+//   const dueLater = () => {
+//     const today = new Date().toISOString().split('T')[0]
+//     return all.filter((item) => !item.completed && item.dueDate > today)
+//   }
+
+  
+//   const toDisplayableList = (list) => {
+//     return list
+//       .map((item, index) => {
+//         const datePart =
+//           item.dueDate === formattedDate(new Date()) ? '' : item.dueDate;
+//         return `[${item.completed ? 'x' : ' '}] ${index + 1}. ${item.title} ${datePart}`;
+//       })
+//       .join('\n');
+//   };
+
+//   return {
+//     all,
+//     add,
+//     markAsComplete,
+//     overdue,
+//     dueToday,
+//     dueLater,
+//     toDisplayableList
+//   }
+// }
+
+// module.exports = todoList
+
+
+// todo.js
+
 const todoList = () => {
-  const all = []
+  const all = [];
 
   const add = (todoItem) => {
-    all.push(todoItem)
-  }
+    all.push(todoItem);
+  };
 
   const markAsComplete = (index) => {
-    all[index].completed = true
-  }
+    all[index].completed = true;
+  };
 
   const overdue = () => {
-    const today = new Date().toISOString().split('T')[0]
-    return all.filter((item) => !item.completed && item.dueDate < today)
-  }
+    const today = new Date().toISOString().split('T')[0];
+    return all.filter((item) => !item.completed && item.dueDate < today);
+  };
 
-  // const dueToday = () => {
-  //   const today = new Date().toISOString().split('T')[0]
-  //   return all.filter(
-  //     (item) =>
-  //       item.dueDate === today &&
-  //       (item.title === 'Pay rent' ? item.completed : !item.completed)
-  //   )
-  // }
   const dueToday = () => {
     const today = new Date().toISOString().split('T')[0];
     return all.filter(
@@ -89,19 +139,10 @@ const todoList = () => {
   };
 
   const dueLater = () => {
-    const today = new Date().toISOString().split('T')[0]
-    return all.filter((item) => !item.completed && item.dueDate > today)
-  }
+    const today = new Date().toISOString().split('T')[0];
+    return all.filter((item) => !item.completed && item.dueDate > today);
+  };
 
-  // const toDisplayableList = (list) => {
-  //   return list
-  //     .map((item) => {
-  //       const datePart =
-  //         item.dueDate === formattedDate(new Date()) ? '' : item.dueDate // Assuming formattedDate is defined elsewhere
-  //       return `[${item.completed ? 'x' : ' '}] ${item.title} ${datePart}`
-  //     })
-  //     .join('\n')
-  // }
   const toDisplayableList = (list) => {
     return list
       .map((item, index) => {
@@ -119,8 +160,8 @@ const todoList = () => {
     overdue,
     dueToday,
     dueLater,
-    toDisplayableList
-  }
-}
+    toDisplayableList,
+  };
+};
 
-module.exports = todoList
+module.exports = todoList;
