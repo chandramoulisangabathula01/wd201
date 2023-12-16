aaadad36e4a41f18f9065130714e3bd4734c08a0
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2023-12-16 20:54:24 +0530

    tye127

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -71,29 +71,46 @@ const todoList = () => {
     return all.filter((item) => !item.completed && item.dueDate < today)
   }
 
+  // const dueToday = () => {
+  //   const today = new Date().toISOString().split('T')[0]
+  //   return all.filter(
+  //     (item) =>
+  //       item.dueDate === today &&
+  //       (item.title === 'Pay rent' ? item.completed : !item.completed)
+  //   )
+  // }
   const dueToday = () => {
-    const today = new Date().toISOString().split('T')[0]
+    const today = new Date().toISOString().split('T')[0];
     return all.filter(
       (item) =>
         item.dueDate === today &&
-        (item.title === 'Pay rent' ? item.completed : !item.completed)
-    )
-  }
+        (item.title === 'Pay rent' ? item.completed : true)
+    );
+  };
 
   const dueLater = () => {
     const today = new Date().toISOString().split('T')[0]
     return all.filter((item) => !item.completed && item.dueDate > today)
   }
 
+  // const toDisplayableList = (list) => {
+  //   return list
+  //     .map((item) => {
+  //       const datePart =
+  //         item.dueDate === formattedDate(new Date()) ? '' : item.dueDate // Assuming formattedDate is defined elsewhere
+  //       return `[${item.completed ? 'x' : ' '}] ${item.title} ${datePart}`
+  //     })
+  //     .join('\n')
+  // }
   const toDisplayableList = (list) => {
     return list
-      .map((item) => {
+      .map((item, index) => {
         const datePart =
-          item.dueDate === formattedDate(new Date()) ? '' : item.dueDate // Assuming formattedDate is defined elsewhere
-        return `[${item.completed ? 'x' : ' '}] ${item.title} ${datePart}`
+          item.dueDate === formattedDate(new Date()) ? '' : item.dueDate;
+        return `[${item.completed ? 'x' : ' '}] ${index + 1}. ${item.title} ${datePart}`;
       })
-      .join('\n')
-  }
+      .join('\n');
+  };
 
   return {
     all,
