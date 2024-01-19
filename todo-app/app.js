22451f95e3a8fe3f550e9a1ed23ff677ee7f00bd
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-01-19 23:32:20 +0530

    script update

--- todo-app/app.js
+++ todo-app/app.js
@@ -50,21 +50,44 @@ const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+const path = require("path")
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-  response.send("Hello World");
-});
+app.set("view engine", "ejs");
+
+app.get("/", async (request, response) => {
+  const allTodos = await Todo.getTodos();
+  if (request.accepts("html")) {
+    response.render("index", {
+      allTodos
+    });
+  } else {
+    response.json({
+      allTodos
+    })
+  }
+  
+})
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/todos", (request, response) => {
+  console.log("Todo list", request.body);
+})
+
+// app.get("/", function (request, response) {
+//   response.send("Hello World");
+// });
 
 app.get("/todos", async function (_request, response) {
-    try {
-      const todos = await Todo.findAll();
-      return response.json(todos);
-    } catch (error) {
-      console.log(error);
-      return response.status(500).json({ error: "Internal Server Error" });
-    }
-  });
+  try {
+    const todos = await Todo.findAll();
+    return response.json(todos);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.get("/todos/:id", async function (request, response) {
   try {
@@ -87,26 +110,26 @@ app.post("/todos", async function (request, response) {
 });
 
 app.put("/todos/:id/setCompletionStatus", async (request, response) => {
-    const todoId = request.params.id;
-  
-    try {
-        const todo = await Todo.findByPk(todoId);
-  
-        if (!todo) {
-            return response.status(404).json({ error: "Todo not found" });
-        }
-  
-        todo.completed = request.body.completed;
-        await todo.save();
-  
-        return response.json({ todo, message: "Todo updated successfully" });
-  
-        
-    } catch (error) {
-        console.error(error);
-        return response.status(500).json({ error: "Internal Server Error" });
+  const todoId = request.params.id;
+
+  try {
+    const todo = await Todo.findByPk(todoId);
+
+    if (!todo) {
+      return response.status(404).json({ error: "Todo not found" });
     }
-  });
+
+    todo.completed = request.body.completed;
+    await todo.save();
+
+    return response.json({ todo, message: "Todo updated successfully" });
+
+
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
@@ -125,7 +148,7 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 //       const deletedTodoCount = await Todo.destroy({
 //         where: { id: todoId },
 //       });
-  
+
 //       if (deletedTodoCount > 0) {
 //         return response.json({ success: true });
 //       } else {
@@ -140,12 +163,12 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 // app.delete("/todos/:id", async function (request, response) {
 //     try {
 //       const todoId = request.params.id;
-  
+
 //       // Using `destroy` method to delete the todo by ID
 //       const deletedTodoCount = await Todo.destroy({
 //         where: { id: todoId },
 //       });
-  
+
 //       // Check if a todo was deleted and send a boolean response
 //       if (deletedTodoCount > 0) {
 //         return response.json({ success: true });
@@ -160,24 +183,24 @@ app.put("/todos/:id/markAsCompleted", async function (request, response) {
 //   });
 
 app.delete("/todos/:id", async (request, response) => {
-    console.log("Delete a todo by ID:", request.params.id)
-    const todoId = request.params.id;
-    const todo = await Todo.findByPk(todoId);
-    try {
-      if (!todo) {
-        return response.status(404).send(false);
-      }
-      else {
-        return response.send(await todo.destroy() ? true : false);
-      }
-      await Todo.remove(request.params.id);
-      return response.json({ success: true });
-    } catch (error) {
-      console.error("Error deleting todo:", error);
-      return response.status(500).json({ success: false, error: "Internal Server Error" });
+  console.log("Delete a todo by ID:", request.params.id)
+  const todoId = request.params.id;
+  const todo = await Todo.findByPk(todoId);
+  try {
+    if (!todo) {
+      return response.status(404).send(false);
     }
-  })
-  
+    else {
+      return response.send(await todo.destroy() ? true : false);
+    }
+    await Todo.remove(request.params.id);
+    return response.json({ success: true });
+  } catch (error) {
+    console.error("Error deleting todo:", error);
+    return response.status(500).json({ success: false, error: "Internal Server Error" });
+  }
+})
+
 
 module.exports = app;
 
