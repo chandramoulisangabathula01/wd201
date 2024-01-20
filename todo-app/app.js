6c8a7e85093c3858f76a37678de34ecdefa9d752
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-01-21 00:30:54 +0530

    l8 try1

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,258 +1,51 @@
-// const { request, response } = require('express')
-// const express = require('express')
-// const app = express()
-// const { Todo } = require("./models")
-// const bodyParser = require('body-parser')
-
-// app.use(bodyParser.json());
-
-
-// app.get("/todos", (request, response) => {
-//     console.log("Todo list")
-// })
-
-// app.post("/todos", async (request, response) => {
-//     console.log("Creating a todo", request.body)
-//     //todo
-
-//     try {
-//         const todo = await Todo.addTodo({ title: request.body.title, dueDate: request.body.dueDate, completed: false })
-//         return response.json(todo)
-//     } catch (error) {
-//         console.log(error)
-//         return response.status(422).json(error)
-
-//     }
-// })
-
-// app.put("/todos/:id/markAsCompleted", async (request, response) => {
-//     console.log("We have to update a todo with ID:", request.params.id)
-//     const todo = await Todo.findByPk(request.params.id)
-//     try {
-//         const updatedTodo = await todo.markAsCompleted()
-//         return response.json(updatedTodo)
-//     } catch (error) {
-//         console.log(error)
-//         return response.status(422).json(error)
-//     }
-
-// })
-
-// app.delete("/todos/:id", (request, response) => {
-//     console.log("Delete a todo by ID: ", request.params.id)
-// })
-
-// module.exports = app;
-
-
-
 // const express = require("express");
 // const app = express();
 // const { Todo } = require("./models");
 // const bodyParser = require("body-parser");
-// const path = require("path")
+// const path = require("path");
+
 // app.use(bodyParser.json());
 
 // app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
 
 // app.get("/", async (request, response) => {
-//   const allTodos = await Todo.getTodos();
-//   if (request.accepts("html")) {
-//     response.render("index", {
-//       allTodos
-//     });
-//   } else {
-//     response.json({
-//       allTodos
-//     })
-//   }
-  
-// })
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get("/todos", (request, response) => {
-//   console.log("Todo list", request.body);
-// })
-
-// // app.get("/", function (request, response) {
-// //   response.send("Hello World");
-// // });
-
-// app.get("/todos", async function (_request, response) {
 //   try {
-//     const todos = await Todo.findAll();
-//     return response.json(todos);
-//   } catch (error) {
-//     console.log(error);
-//     return response.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
+//     const overdue = await Todo.getOverdueTodos();
+//     const duetoday = await Todo.getDueTodayTodos();
+//     const duelater = await Todo.getDueLaterTodos();
 
-// app.get("/todos/:id", async function (request, response) {
-//   try {
-//     const todo = await Todo.findByPk(request.params.id);
-//     return response.json(todo);
-//   } catch (error) {
-//     console.log(error);
-//     return response.status(422).json(error);
-//   }
-// });
-
-// app.post("/todos", async function (request, response) {
-//   try {
-//     const todo = await Todo.addTodo(request.body);
-//     return response.json(todo);
-//   } catch (error) {
-//     console.log(error);
-//     return response.status(422).json(error);
-//   }
-// });
-
-// app.put("/todos/:id/setCompletionStatus", async (request, response) => {
-//   const todoId = request.params.id;
-
-//   try {
-//     const todo = await Todo.findByPk(todoId);
-
-//     if (!todo) {
-//       return response.status(404).json({ error: "Todo not found" });
+//     if (request.accepts("html")) {
+//       response.render("index.ejs", {
+//         overdue,
+//         duetoday,
+//         duelater,
+//       });
+//     } else {
+//       response.json({
+//         overdue,
+//         duetoday,
+//         duelater,
+//       });
 //     }
-
-//     todo.completed = request.body.completed;
-//     await todo.save();
-
-//     return response.json({ todo, message: "Todo updated successfully" });
-
-
 //   } catch (error) {
 //     console.error(error);
-//     return response.status(500).json({ error: "Internal Server Error" });
+//     response.status(500).json({ error: "Internal Server Error" });
 //   }
 // });
 
-// app.put("/todos/:id/markAsCompleted", async function (request, response) {
-//   const todo = await Todo.findByPk(request.params.id);
-//   try {
-//     const updatedTodo = await todo.markAsCompleted();
-//     return response.json(updatedTodo);
-//   } catch (error) {
-//     console.log(error);
-//     return response.status(422).json(error);
-//   }
+// app.get("/", function (request, response) {
+//   response.send("Hello World");
 // });
-
-// // app.delete("/todos/:id", async function (request, response) {
-// //     try {
-// //       const todoId = request.params.id;
-// //       const deletedTodoCount = await Todo.destroy({
-// //         where: { id: todoId },
-// //       });
-
-// //       if (deletedTodoCount > 0) {
-// //         return response.json({ success: true });
-// //       } else {
-// //         return response.status(404).json({ error: "Todo not found" });
-// //       }
-// //     } catch (error) {
-// //       console.log(error);
-// //       return response.status(500).json({ error: "Internal Server Error" });
-// //     }
-// //   });
-
-// // app.delete("/todos/:id", async function (request, response) {
-// //     try {
-// //       const todoId = request.params.id;
-
-// //       // Using `destroy` method to delete the todo by ID
-// //       const deletedTodoCount = await Todo.destroy({
-// //         where: { id: todoId },
-// //       });
-
-// //       // Check if a todo was deleted and send a boolean response
-// //       if (deletedTodoCount > 0) {
-// //         return response.json({ success: true });
-// //       } else {
-// //         // If no todo was found with the given ID, respond accordingly
-// //         return response.json({ success: false });
-// //       }
-// //     } catch (error) {
-// //       console.error(error);
-// //       return response.status(500).json({ error: "Internal Server Error" });
-// //     }
-// //   });
-
-// app.delete("/todos/:id", async (request, response) => {
-//   console.log("Delete a todo by ID:", request.params.id)
-//   const todoId = request.params.id;
-//   const todo = await Todo.findByPk(todoId);
-//   try {
-//     if (!todo) {
-//       return response.status(404).send(false);
-//     }
-//     else {
-//       return response.send(await todo.destroy() ? true : false);
-//     }
-//     await Todo.remove(request.params.id);
-//     return response.json({ success: true });
-//   } catch (error) {
-//     console.error("Error deleting todo:", error);
-//     return response.status(500).json({ success: false, error: "Internal Server Error" });
-//   }
-// })
-
-
-// module.exports = app;
-
-
-
-
-
-
-// new
-
-
-
-
-// const express = require("express");
-// // const { sequelize, testConnection } = require('./db');
-// const app = express();
-// const { Todo } = require("./models");
-// // const port = process.env.PORT || 3000;
-// const path = require("path");
-// const bodyParser = require("body-parser");
-
-// // testConnection();
-
-// app.use(bodyParser.json());
-
-
-// app.set("view engine", "ejs");
-
-// app.get("/", async (request,response) =>{
-//   const allTodos = await Todo.getTodos();
-//   if(request.accepts("html")){
-//     response.render('index',{allTodos});
-//     return;
-//   }else{
-//     response.json({
-//       allTodos
-//     })
-//   }
-  
-// })
-
-// app.use(express.static(path.join(__dirname,'public')));
 
 // app.get("/todos", async function (_request, response) {
 //   console.log("Processing list of all Todos ...");
-//   try{
-//     const todo = await Todo.findAll();
-//     return response.json(todo);
-
-//   } catch(error){
-//     console.log(error);
-//     return response.status(422).json(error);
+//   try {
+//     const todos = await Todo.findAll();
+//     response.send(todos);
+//   } catch (error) {
+//     console.error(error);
+//     response.status(500).json({ error: "Internal Server Error" });
 //   }
 // });
 
@@ -261,18 +54,17 @@
 //     const todo = await Todo.findByPk(request.params.id);
 //     return response.json(todo);
 //   } catch (error) {
-//     console.log(error);
+//     console.error(error);
 //     return response.status(422).json(error);
 //   }
 // });
-
 
 // app.post("/todos", async function (request, response) {
 //   try {
 //     const todo = await Todo.addTodo(request.body);
 //     return response.json(todo);
 //   } catch (error) {
-//     console.log(error);
+//     console.error(error);
 //     return response.status(422).json(error);
 //   }
 // });
@@ -283,138 +75,155 @@
 //     const updatedTodo = await todo.markAsCompleted();
 //     return response.json(updatedTodo);
 //   } catch (error) {
-//     console.log(error);
+//     console.error(error);
 //     return response.status(422).json(error);
 //   }
 // });
 
 // app.delete("/todos/:id", async function (request, response) {
 //   console.log("We have to delete a Todo with ID: ", request.params.id);
-//   const deleteFlag = await Todo.destroy({ where: { id: request.params.id}})
-//    response.send(deleteFlag?true:false)
+//   try {
+//     if (await Todo.findByPk(request.params.id)) {
+//       await Todo.destroy({
+//         where: {
+//           id: request.params.id,
+//         },
+//       });
+//       if (await Todo.findByPk(request.params.id)) {
+//         response.send(false);
+//       } else {
+//         response.send(true);
+//       }
+//     } else {
+//       response.send(false);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     response.status(500).json({ error: "Internal Server Error" });
+//   }
 // });
-
 
 // module.exports = app;
 
 
 
 
-// old
 
 const express = require("express");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
+
+// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
+// Set view engine and static file directory
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
+// Route to handle root URL
 app.get("/", async (request, response) => {
-  const allTodos = await Todo.getTodos();
-  if (request.accepts("html")) {
-    response.render("index", {
-      allTodos
-    });
-  } else {
-    response.json({
-      allTodos
-    })
-  }
-  
-})
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("/todos", (request, response) => {
-  console.log("Todo list", request.body);
-})
-
-
-
-app.get("/todos", async function (_request, response) {
   try {
-    const todos = await Todo.findAll();
-    return response.json(todos);
+    // Retrieve overdue, due today, and due later todos
+    const overdue = await Todo.getOverdueTodos();
+    const duetoday = await Todo.getDueTodayTodos();
+    const duelater = await Todo.getDueLaterTodos();
+
+    // Check if the client accepts HTML, render the HTML view; otherwise, send JSON
+    if (request.accepts("html")) {
+      response.render("index.ejs", {
+        overdue,
+        duetoday,
+        duelater,
+      });
+    } else {
+      response.json({
+        overdue,
+        duetoday,
+        duelater,
+      });
+    }
   } catch (error) {
-    console.log(error);
-    return response.status(500).json({ error: "Internal Server Error" });
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
 });
 
+// Default route for root URL, responds with "Hello World"
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
+
+// Route to get all todos
+app.get("/todos", async function (_request, response) {
+  console.log("Processing list of all Todos ...");
+  try {
+    const todos = await Todo.findAll();
+    response.send(todos);
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Route to get a specific todo by ID
 app.get("/todos/:id", async function (request, response) {
   try {
     const todo = await Todo.findByPk(request.params.id);
     return response.json(todo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
+// Route to add a new todo
 app.post("/todos", async function (request, response) {
   try {
     const todo = await Todo.addTodo(request.body);
     return response.json(todo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
-app.put("/todos/:id/setCompletionStatus", async (request, response) => {
-  const todoId = request.params.id;
-
-  try {
-    const todo = await Todo.findByPk(todoId);
-
-    if (!todo) {
-      return response.status(404).json({ error: "Todo not found" });
-    }
-
-    todo.completed = request.body.completed;
-    await todo.save();
-
-    return response.json({ todo, message: "Todo updated successfully" });
-
-
-  } catch (error) {
-    console.error(error);
-    return response.status(500).json({ error: "Internal Server Error" });
-  }
-});
-
+// Route to mark a todo as completed
 app.put("/todos/:id/markAsCompleted", async function (request, response) {
   const todo = await Todo.findByPk(request.params.id);
   try {
     const updatedTodo = await todo.markAsCompleted();
     return response.json(updatedTodo);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return response.status(422).json(error);
   }
 });
 
-
-app.delete("/todos/:id", async (request, response) => {
-  console.log("Delete a todo by ID:", request.params.id)
-  const todoId = request.params.id;
-  const todo = await Todo.findByPk(todoId);
+// Route to delete a todo by ID
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
   try {
-    if (!todo) {
-      return response.status(404).send(false);
+    if (await Todo.findByPk(request.params.id)) {
+      await Todo.destroy({
+        where: {
+          id: request.params.id,
+        },
+      });
+      if (await Todo.findByPk(request.params.id)) {
+        response.send(false);
+      } else {
+        response.send(true);
+      }
+    } else {
+      response.send(false);
     }
-    else {
-      return response.send(await todo.destroy() ? true : false);
-    }
-    await Todo.remove(request.params.id);
-    return response.json({ success: true });
   } catch (error) {
-    console.error("Error deleting todo:", error);
-    return response.status(500).json({ success: false, error: "Internal Server Error" });
+    console.error(error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
-})
+});
 
-
+// Export the Express app
 module.exports = app;
--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,32 +1,225 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Todo extends Model {
+    
+//     static associate(models) {
+//       // define association here
+//     }
+//     static addTodo({ title, dueDate }) {
+//       return this.create({ title: title, dueDate: dueDate, completed: false })
+//     }
+
+//     static getTodos(){
+//       return this.findAll();
+//     }
+
+//     markAsCompleted() {
+//       return this.update({ completed: true })
+//     }
+//   }
+//   Todo.init({
+//     title: DataTypes.STRING,
+//     dueDate: DataTypes.DATEONLY,
+//     completed: DataTypes.BOOLEAN
+//   }, {
+//     sequelize,
+//     modelName: 'Todo',
+//   });
+//   return Todo;
+// };
+
+
+
+
+
+// "use strict";
+// const { Model, DataTypes, Op } = require("sequelize");
+// module.exports = (sequelize, DataTypes) => {
+//   class Todo extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+
+//     static addTodo({ title, dueDate }) {
+//       return this.create({ title: title, dueDate: dueDate, completed: false });
+//     }
+
+//     markAsCompleted() {
+//       return this.update({ completed: true });
+//     }
+
+//     static async getOverdueTodos() {
+//       try {
+//         const overdueTodos = await Todo.findAll({
+//           where: {
+//             dueDate: {
+//               [Op.lt]: new Date(),
+//             },
+//           },
+//         });
+//         return overdueTodos;
+//       } catch (error) {
+//         console.error('Error getting overdue todos:', error);
+//         throw error;
+//       }
+//     }
+
+//     static async getDueTodayTodos() {
+//       try {
+//         const dueTodayTodos = await Todo.findAll({
+//           where: {
+//             dueDate: {
+//               [Op.between]: [new Date(), new Date(new Date().setHours(23, 59, 59, 999))],
+//             },
+//           },
+//         });
+//         return dueTodayTodos;
+//       } catch (error) {
+//         console.error('Error getting due today todos:', error);
+//         throw error;
+//       }
+//     }
+
+//     static async getDueLaterTodos() {
+//       try {
+//         const dueLaterTodos = await Todo.findAll({
+//           where: {
+//             dueDate: {
+//               [Op.gt]: new Date(),
+//             },
+//           },
+//         });
+//         return dueLaterTodos;
+//       } catch (error) {
+//         console.error('Error getting due later todos:', error);
+//         throw error;
+//       }
+//     }
+
+//     static async getTodos() {
+//       const todos = await Todo.findAll();
+//       return todos;
+//     }
+//   }
+//   Todo.init(
+//     {
+//       title: DataTypes.STRING,
+//       dueDate: DataTypes.DATEONLY,
+//       completed: DataTypes.BOOLEAN,
+//     },
+//     {
+//       sequelize,
+//       modelName: "Todo",
+//     },
+//   );
+//   return Todo;
+// };
+
+
+
+
+
+"use strict";
+
+const { Model, DataTypes, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
-    
+    // Helper method for defining associations.
+    // This method is not a part of Sequelize lifecycle.
+    // The `models/index` file will call this method automatically.
     static associate(models) {
-      // define association here
+      // Define associations here if needed.
     }
+
+    // Method to add a new todo
     static addTodo({ title, dueDate }) {
-      return this.create({ title: title, dueDate: dueDate, completed: false })
+      return this.create({ title, dueDate, completed: false });
     }
 
-    static getTodos(){
-      return this.findAll();
-    }
-
+    // Method to mark a todo as completed
     markAsCompleted() {
-      return this.update({ completed: true })
+      return this.update({ completed: true });
+    }
+
+    // Method to get overdue todos
+    static async getOverdueTodos() {
+      try {
+        const overdueTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.lt]: new Date(),
+            },
+          },
+        });
+        return overdueTodos;
+      } catch (error) {
+        console.error('Error getting overdue todos:', error);
+        throw error;
+      }
+    }
+
+    // Method to get todos due today
+    static async getDueTodayTodos() {
+      try {
+        const dueTodayTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.between]: [new Date(), new Date(new Date().setHours(23, 59, 59, 999))],
+            },
+          },
+        });
+        return dueTodayTodos;
+      } catch (error) {
+        console.error('Error getting due today todos:', error);
+        throw error;
+      }
+    }
+
+    // Method to get todos due later
+    static async getDueLaterTodos() {
+      try {
+        const dueLaterTodos = await Todo.findAll({
+          where: {
+            dueDate: {
+              [Op.gt]: new Date(),
+            },
+          },
+        });
+        return dueLaterTodos;
+      } catch (error) {
+        console.error('Error getting due later todos:', error);
+        throw error;
+      }
+    }
+
+    // Method to get all todos
+    static async getTodos() {
+      const todos = await Todo.findAll();
+      return todos;
     }
   }
-  Todo.init({
-    title: DataTypes.STRING,
-    dueDate: DataTypes.DATEONLY,
-    completed: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'Todo',
-  });
+
+  // Initialize the Todo model
+  Todo.init(
+    {
+      title: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
+      completed: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Todo",
+    }
+  );
+
   return Todo;
 };
