1fbd7bf885132c999d740e0d97e316ec4971346d
chandramoulisangabathula01 <chandramoulisangabathula01@gmail.com>
2024-01-21 00:33:13 +0530

    l8 try2

--- todo-app/config/config.js
+++ todo-app/config/config.js
@@ -1,31 +1,26 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// module.exports = {
-//   development: {
-//     username: 'postgres',
-//     password: '123456',
-//     database: 'database_development',
-//     host: '127.0.0.1',
-//     dialect: 'postgres',
-//   },
-//   test: {
-//     username: 'postgres',
-//     password: '123456',
-//     database: 'wd-todo-test',
-//     host: '127.0.0.1',
-//     dialect: 'postgres',
-//     logging: false,
-//   },
-//   production: {
-//    "use_env_variable": "DATABASE_URL",
-//    "dialect":"prostgres"
-//   }
-// };
-// const sequelize = new Sequelize('database', 'username', 'password', {
-//   host: 'localhost',
-//   port: 5432,
-//   dialect: 'postgres',
-// });
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: '123456',
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: '123456',
+    database: 'wd-todo-test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+    logging: false,
+  },
+  production: {
+   "use_env_variable": "DATABASE_URL",
+   "dialect":"prostgres"
+  }
+};
 
 
 
